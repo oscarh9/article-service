6e53f32fbcd116f7b3d8f80607a3e6b45fabd1b7
oscarh9 <oscarherenciasakkis@gmail.com>
2025-07-29 02:40:53 +0200

    created Article entity #1

---
+++ src/main/java/com/fife/article_service/model/Article.java
@@ -0,0 +1,23 @@
+package com.fife.article_service.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "articles")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+    private LocalDateTime createdAt;
+}
