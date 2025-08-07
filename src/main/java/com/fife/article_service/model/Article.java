5d7914edacdbfa45929819f93445c20cdddbea5a
oscarh9 <oscarherenciasakkis@gmail.com>
2025-08-07 12:55:12 +0200

    rename Article and add updatedAt attribute #8

--- src/main/java/com/fife/article_service/model/Article.java
+++ src/main/java/com/fife/article_service/model/Article.java
@@ -1,20 +1,20 @@
 package com.fife.article_service.model;
 
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class ArticleModel {
+public class Article {
     private Long id;
     private String title;
     private String content;
     private String author;
     private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
