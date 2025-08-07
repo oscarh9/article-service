8e92a439a5df048d3d421318020807ff482c3e28
oscarh9 <oscarherenciasakkis@gmail.com>
2025-08-07 12:53:45 +0200

    refactor ArticleDao interface #8

--- src/main/java/com/fife/article_service/dao/ArticleDao.java
+++ src/main/java/com/fife/article_service/dao/ArticleDao.java
@@ -1,13 +1,15 @@
 package com.fife.article_service.dao;
 
-import com.fife.article_service.model.ArticleModel;
-
+import com.fife.article_service.model.Article;
 import java.util.List;
 import java.util.Optional;
 
 public interface ArticleDao {
-    ArticleModel save(ArticleModel article);
-    List<ArticleModel> findAll();
-    Optional<ArticleModel> findById(Long id);
-    void delete(ArticleModel article);
+    Article save(Article article);
+
+    List<Article> findAll();
+
+    Optional<Article> findById(Long id);
+
+    void delete(Article article);
 }
