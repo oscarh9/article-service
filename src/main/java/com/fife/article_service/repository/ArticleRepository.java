95d87f562a69dec0e377bc5a126f6bce1534d07c
oscarh9 <oscarherenciasakkis@gmail.com>
2025-08-09 02:32:57 +0200

    modify repository implementing MongoRepository #9

--- src/main/java/com/fife/article_service/repository/ArticleRepository.java
+++ src/main/java/com/fife/article_service/repository/ArticleRepository.java
@@ -1,7 +1,7 @@
 package com.fife.article_service.repository;
 
 import com.fife.article_service.entity.ArticleEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface ArticleRepository extends JpaRepository<ArticleEntity,Long> {
+public interface ArticleRepository extends MongoRepository<ArticleEntity, String> {
 }
