7de43144b01ed1bec1183108c9874e751c63c08c
oscarh9 <oscarherenciasakkis@gmail.com>
2025-08-09 02:29:35 +0200

    modify model using Mongo annotations #9

--- src/main/java/com/fife/article_service/model/Article.java
+++ src/main/java/com/fife/article_service/model/Article.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class Article {
-    private Long id;
+    private String id;
     private String title;
     private String content;
     private String author;
